fa3537fb89f7c331dc97e83ec03a2a2df7d63bc3
Tega Adigu <tega.adigu@gmail.com>
2016-04-03 14:30:49 +0800

    mobile first implementation

--- resources/views/component/top-menu.blade.php
+++ resources/views/component/top-menu.blade.php
@@ -1,24 +1,9 @@
 <div class="top-menu clearfix">
-    {{--<div class="menu-header clearfix">--}}
-    {{--<div class="menu-header-image">--}}
-    {{--<img src="/img/ticket-icon.png">--}}
-    {{--</div>--}}
-    {{--<div class="menu-header-text">--}}
-    {{--INSTANT DIGITAL TICKETING--}}
-    {{--</div>--}}
-    {{--</div>--}}
-    {{--<div>--}}
-    {{--GURU--}}
-    {{--</div>--}}
-    {{--<div class="become-operator clearfix">--}}
-    {{--<div>--}}
-    {{--WANT TO DRIVE WITH GURU?--}}
-    {{--</div>--}}
-    {{--<button class="btn-danger btn-sm"> BECOME AN OPERATOR</button>--}}
-    {{--</div>--}}
     <span class="site-phrase">
         <img src="/img/ticket-icon.png">
-        INSTANT DIGITAL TICKETING
+        <span>
+             INSTANT DIGITAL TICKETING
+        </span>
     </span>
     <span class="title">
        <i class="fa fa-map-marker"></i>
